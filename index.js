7bd7e53f264454a72d790ddf4b863664f1920f77
Dalia Jaber <daliajaber96@gmail.com>
2022-10-30 10:38:25 +0200

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+function destructivelyAppendCat(name){
+    // const cat = [...cats];
+    cats.push(name);
+    console.log(cats);
+    return cats;
+}
+// destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name){
+    // const cat = [...cats];
+    cats.unshift(name);
+    console.log(cats);
+    return cats;
+}
+// destructivelyPrependCat("Bob");
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    const cat = [...cats];
+    cat.push("Broom");
+    return cat;
+}
+
+function prependCat(name){
+    const cat = [...cats];
+    cat.unshift("Arnold");
+    return cat;
+}
+
+function removeLastCat(){
+    const cat = [...cats];
+    cat.pop();
+    return cat;
+}
+
+function removeFirstCat(){
+    const cat = [...cats];
+    cat.shift();
+    return cat;
+}
